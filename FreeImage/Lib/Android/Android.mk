78d58704b1ecc842663790ebda5d3adcce66ae67
xie_dev <xie_dev@163.com>
2014-05-16 22:42:39 +0800

    add freeimage.

---
+++ FreeImage/Lib/Android/Android.mk
@@ -0,0 +1,159 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := FreeImage
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../Source \
+    $(LOCAL_PATH)/../Source \
+    $(LOCAL_PATH)/../Source/DeprecationManager \
+    $(LOCAL_PATH)/../Source/FreeImage \
+    $(LOCAL_PATH)/../Source/FreeImageLib \
+    $(LOCAL_PATH)/../Source/FreeImageToolkit \
+    $(LOCAL_PATH)/../Source/LibJPEG \
+    $(LOCAL_PATH)/../Source/LibPNG	\
+    $(LOCAL_PATH)/../Source/Metadata \
+    $(LOCAL_PATH)/../Source/ZLib \
+
+LOCAL_SRC_FILES = ../Source/FreeImage/BitmapAccess.cpp \
+../Source/FreeImage/ColorLookup.cpp	\
+../Source/FreeImage/FreeImage.cpp \
+../Source/FreeImage/FreeImageC.c \
+../Source/FreeImage/FreeImageIO.cpp \
+../Source/FreeImage/GetType.cpp \
+../Source/FreeImage/MemoryIO.cpp \
+../Source/FreeImage/PixelAccess.cpp \
+../Source/FreeImage/MNGHelper.cpp \
+../Source/FreeImage/Plugin.cpp \
+../Source/FreeImage/PluginBMP.cpp \
+../Source/FreeImage/PluginDDS.cpp \
+../Source/FreeImage/PluginGIF.cpp \
+../Source/FreeImage/PluginJPEG.cpp \
+../Source/FreeImage/PluginPNG.cpp \
+../Source/FreeImage/PluginPSD.cpp \
+../Source/FreeImage/PSDParser.cpp \
+../Source/FreeImage/Conversion.cpp \
+../Source/FreeImage/Conversion16_555.cpp \
+../Source/FreeImage/Conversion16_565.cpp \
+../Source/FreeImage/Conversion24.cpp \
+../Source/FreeImage/Conversion32.cpp \
+../Source/FreeImage/Conversion4.cpp \
+../Source/FreeImage/Conversion8.cpp \
+../Source/FreeImage/ConversionFloat.cpp \
+../Source/FreeImage/ConversionRGB16.cpp \
+../Source/FreeImage/ConversionRGBF.cpp \
+../Source/FreeImage/ConversionType.cpp \
+../Source/FreeImage/ConversionUINT16.cpp \
+../Source/FreeImage/Halftoning.cpp \
+../Source/FreeImage/tmoColorConvert.cpp \
+../Source/FreeImage/tmoDrago03.cpp \
+../Source/FreeImage/tmoFattal02.cpp \
+../Source/FreeImage/tmoReinhard05.cpp \
+../Source/FreeImage/ToneMapping.cpp \
+../Source/FreeImage/NNQuantizer.cpp \
+../Source/FreeImage/WuQuantizer.cpp \
+../Source/DeprecationManager/Deprecated.cpp \
+../Source/DeprecationManager/DeprecationMgr.cpp \
+../Source/FreeImage/CacheFile.cpp \
+../Source/FreeImage/MultiPage.cpp \
+../Source/FreeImage/ZLibInterface.cpp \
+../Source/Metadata/Exif.cpp \
+../Source/Metadata/FIRational.cpp \
+../Source/Metadata/FreeImageTag.cpp \
+../Source/Metadata/IPTC.cpp \
+../Source/Metadata/TagConversion.cpp \
+../Source/Metadata/TagLib.cpp \
+../Source/FreeImageToolkit/Background.cpp \
+../Source/FreeImageToolkit/BSplineRotate.cpp \
+../Source/FreeImageToolkit/Channels.cpp \
+../Source/FreeImageToolkit/ClassicRotate.cpp \
+../Source/FreeImageToolkit/Colors.cpp \
+../Source/FreeImageToolkit/CopyPaste.cpp \
+../Source/FreeImageToolkit/Display.cpp \
+../Source/FreeImageToolkit/Flip.cpp \
+../Source/FreeImageToolkit/JPEGTransform.cpp \
+../Source/FreeImageToolkit/MultigridPoissonSolver.cpp \
+../Source/FreeImageToolkit/Rescale.cpp \
+../Source/FreeImageToolkit/Resize.cpp \
+../Source/LibJPEG/./jaricom.c \
+../Source/LibJPEG/jcapimin.c \
+../Source/LibJPEG/jcapistd.c \
+../Source/LibJPEG/./jcarith.c \
+../Source/LibJPEG/jccoefct.c \
+../Source/LibJPEG/jccolor.c \
+../Source/LibJPEG/jcdctmgr.c \
+../Source/LibJPEG/jchuff.c \
+../Source/LibJPEG/jcinit.c \
+../Source/LibJPEG/jcmainct.c \
+../Source/LibJPEG/jcmarker.c \
+../Source/LibJPEG/jcmaster.c \
+../Source/LibJPEG/jcomapi.c \
+../Source/LibJPEG/jcparam.c \
+../Source/LibJPEG/jcprepct.c \
+../Source/LibJPEG/jcsample.c \
+../Source/LibJPEG/jctrans.c \
+../Source/LibJPEG/jdapimin.c \
+../Source/LibJPEG/jdapistd.c \
+../Source/LibJPEG/./jdarith.c \
+../Source/LibJPEG/jdatadst.c \
+../Source/LibJPEG/jdatasrc.c \
+../Source/LibJPEG/jdcoefct.c \
+../Source/LibJPEG/jdcolor.c \
+../Source/LibJPEG/jddctmgr.c \
+../Source/LibJPEG/jdhuff.c \
+../Source/LibJPEG/jdinput.c \
+../Source/LibJPEG/jdmainct.c \
+../Source/LibJPEG/jdmarker.c \
+../Source/LibJPEG/jdmaster.c \
+../Source/LibJPEG/jdmerge.c \
+../Source/LibJPEG/jdpostct.c \
+../Source/LibJPEG/jdsample.c \
+../Source/LibJPEG/jdtrans.c \
+../Source/LibJPEG/jerror.c \
+../Source/LibJPEG/jfdctflt.c \
+../Source/LibJPEG/jfdctfst.c \
+../Source/LibJPEG/jfdctint.c \
+../Source/LibJPEG/jidctflt.c \
+../Source/LibJPEG/jidctfst.c \
+../Source/LibJPEG/jidctint.c \
+../Source/LibJPEG/jmemmgr.c \
+../Source/LibJPEG/jmemnobs.c \
+../Source/LibJPEG/jquant1.c  \
+../Source/LibJPEG/jquant2.c \
+../Source/LibJPEG/jutils.c \
+../Source/LibJPEG/transupp.c \
+../Source/LibPNG/png.c \
+../Source/LibPNG/pngerror.c \
+../Source/LibPNG/pngget.c \
+../Source/LibPNG/pngmem.c \
+../Source/LibPNG/pngpread.c \
+../Source/LibPNG/pngread.c \
+../Source/LibPNG/pngrio.c \
+../Source/LibPNG/pngrtran.c \
+../Source/LibPNG/pngrutil.c \
+../Source/LibPNG/pngset.c \
+../Source/LibPNG/pngtrans.c \
+../Source/LibPNG/pngwio.c \
+../Source/LibPNG/pngwrite.c \
+../Source/LibPNG/pngwtran.c \
+../Source/LibPNG/pngwutil.c \
+../Source/ZLib/./adler32.c \
+../Source/ZLib/./compress.c \
+../Source/ZLib/./crc32.c \
+../Source/ZLib/./deflate.c \
+../Source/ZLib/./gzclose.c \
+../Source/ZLib/./gzlib.c \
+../Source/ZLib/./gzread.c  \
+../Source/ZLib/./gzwrite.c \
+../Source/ZLib/./infback.c \
+../Source/ZLib/./inffast.c \
+../Source/ZLib/./inflate.c  \
+../Source/ZLib/./inftrees.c \
+../Source/ZLib/./trees.c \
+../Source/ZLib/./uncompr.c \
+../Source/ZLib/./zutil.c
+
+LOCAL_CFLAGS := -DFREEIMAGE_LIB
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ FreeImage/Lib/Android/Application.mk
@@ -0,0 +1,6 @@
+APP_OPTIM := release
+APP_PLATFORM := android-8
+APP_STL := gnustl_static
+APP_CPPFLAGS := -frtti -fexceptions -DANDROID
+APP_ABI := armeabi armeabi-v7a x86
+APP_MODULES := FreeImage
